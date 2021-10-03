cab3e5b8735c55d5a503b5e55a27e2b95b2b9523
StroeAndreX <stroeandrex.studio@gmail.com>
2021-10-03 16:29:39 +0200

    Platform

--- Assets/Scripts/PlayerMoney.cs
+++ Assets/Scripts/PlayerMoney.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlayerMoney : MonoBehaviour
 {
-    Currency currency = new Currency(); 
-        
+    public Currency currency;
+    PlayerMovements pMove = new PlayerMovements();
 
     // Start is called before the first frame update
     void Start()
     {
-        currency.quantity = 1; 
+        currency.quantity = 1;
+        pMove = this.GetComponent<PlayerMovements>(); 
     }
 
     // Update is called once per frame
@@ -19,5 +20,11 @@ public class PlayerMoney : MonoBehaviour
         Debug.Log(currency.conversion());
 
         if(Input.GetKeyDown(KeyCode.Q)) currency.changeCryptoValue();
+        if (Input.GetKeyDown(KeyCode.P)) transformPlatform(); 
+    }
+
+    public void transformPlatform()
+    {
+        pMove.sittingPlatform.GetComponent<Platform>().transformPlatform(Platform.Type.Mine);
     }
 }
